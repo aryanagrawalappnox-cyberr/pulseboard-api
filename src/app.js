0e0a6bbb6e71a0f03f81ef2c85cf3b407e22c839
abhishekappnox <abhisheks.appnox@gmail.com>
2026-08-07 00:04:46 -0700

    Complete Phase 1 Express foundation

--- src/app.js
+++ src/app.js
@@ -6,7 +6,24 @@ app.use((req, res, next) => {
     
     const timeStamps = new Date();
     console.log(`[${timeStamps.toISOString()}] ${req.method} ${req.originalUrl}`);
-    // next();
+     next();
 });
+
+app.get('/health', (req,res) =>{
+   res.json({
+   status: "ok"
+});
+});
+
+
+app.use((err,req,res,next) => {
+   console.error(err);
+   res.status(500).json({
+    success: false,
+    message: err.message
+})
+});
+
+
 
 export default app;
